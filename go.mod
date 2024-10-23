7d94a7fe77ac062d661cf78d1fb5c69255b7b059
80red <93551209+80RED@users.noreply.github.com>
2024-10-22 22:04:39 -0400

    Render html with Templ and htmx

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module booktracker
+module book-tracker
 
 go 1.22.2
+
+require github.com/a-h/templ v0.2.778
--- main.go
+++ main.go
@@ -3,30 +3,47 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"book-tracker/components"
+
+	"github.com/a-h/templ"
 )
 
-func staticFileHandler() http.Handler {
+func assetHandler() http.Handler {
 	fs := http.FileServer(http.Dir("assets"))
-	return http.StripPrefix("/static/", fs)
+	return http.StripPrefix("/assets/", fs)
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+func booksHandler(w http.ResponseWriter, r *http.Request) {
+	content := components.Books()
+	renderContent(w, r, content)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	content := components.About()
+	renderContent(w, r, content)
+}
+
+func settingsHandler(w http.ResponseWriter, r *http.Request) {
+	content := components.Settings()
+	renderContent(w, r, content)
+}
+
+func renderContent(w http.ResponseWriter, r *http.Request, content templ.Component) {
+	if r.Header.Get("HX-Request") == "true" {
+		templ.Handler(content).ServeHTTP(w, r)
 	} else {
-		http.ServeFile(w, r, "index.html")
+		templ.Handler(components.Base(content)).ServeHTTP(w, r)
 	}
 }
 
 func main() {
-
-	// Set up file server for static assets
-	http.Handle("/static/", staticFileHandler())
-
-	// Handler for the root path
-	http.HandleFunc("/", rootHandler)
+	http.Handle("/assets/", assetHandler())
+	http.HandleFunc("/", booksHandler)
+	http.HandleFunc("/books", booksHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/settings", settingsHandler)
 
 	log.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", nil)
 }
